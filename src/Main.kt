ae6ace72b3a5b233190e9e6be71843713cd8bc65
AdalrosaLuis <rosa.adalrosa@gmail.com>
2025-05-28 20:28:46 +0100

    erthedrthrthdrfthrfth

--- src/Main.kt
+++ src/Main.kt
@@ -10,12 +10,14 @@ var valorInvestido = emptyArray<String>()
 var valorAtual = emptyArray<String>()
 var quantidade = emptyArray<String>()
 var rentabilidade = emptyArray<String>()
-var investimentos: Array<Array<String>?> = arrayOfNulls(99)
+var investimentos = arrayOfNulls<Array<String>>(100)
 var configuracoes = emptyArray<String>()
 var liquidacoes = emptyArray<Double>()
+var nomeInvestimento= emptyArray<String>()
+var numeroLinhas = emptyArray<String>()
+var indice= emptyArray<Int>()
 
-
-fun obterMenu(): String {
+fun obterMenu():String {
 	val menuCarteira = "#  Comando\n" +
 			"1. Consultar\n" +
 			"2. Adicionar\n" +
@@ -24,199 +26,148 @@ fun obterMenu(): String {
 			"5. Guardar\n" +
 			"9. Configuracoes\n" +
 			"0. Sair\n"
+	
 	return menuCarteira
 }
-
-fun lerConfiguracoes(fileName: String, configuracoes: Array<String>): Boolean {
-	return false
-}
-
-fun guardarConfiguracoes(fileName: String, liquidacoes: Array<String>) {
-
-}
-
-fun lerLiquidacoes(fileName: String, liquidacoes: Array<Double>): Boolean {
-	
-	return false
-}
-
-fun guardarLiquidacoes(fileName: String, liquidacoes: Array<Double>) {
-
-}
-
-fun lerInvestimentos(fileName: String, investimentos: Array<Array<String>?>): Boolean {
-	
+fun lerConfiguracoes(fileName: String, configuracoes:Array<String>):Boolean {
 	return false
 	
 }
+fun guardarConfiguracoes(fileName: String, liquidacoes:Array<String>) {
 
-//fun consultarInvestimentos(
-//	investimentos: Array<Array<String>?>,
-//	configuracoes: Array<String>,
-//	liquidacoes: Array<Double>,
-//): String {
-//	return ""
-//}
-
-fun consultarInvestimentos(investimentos: Array<Array<String>?>, configuracoes: Array<String>, liquidacoes: Array<Double>
-): String {
-	var resultado = "# | Nome     | Data       | Investido | Atual    | Quantidade | Rentabilidade\n"
-	resultado += "-------------------------------------------------------------------------------\n"
-	
-	var lucroTotal = 0.0
-	
-	for ((indice, investimento) in investimentos.withIndex()) {
-		if (investimento != null) {
-			val nome = investimento[0]
-			val data = investimento[1]
-			val investido = investimento[2].toDouble()
-			val atual = investimento[3].toDouble()
-			val quantidade = investimento[4].toDouble()
-			
-			val rentabilidade = if (investido != 0.0)
-				((atual - investido) / investido) * 100
-			else
-				0.0
-			
-			// Linha formatada simples
-			resultado += "${indice + 1} | ${nome.padEnd(8)} | ${data} | ${"%.2f".format(investido)} € | " +
-					"${"%.2f".format(atual)} € | ${"%.2f".format(quantidade)} | " +
-					"${"%.1f".format(rentabilidade)} %\n"
-			
-			lucroTotal += (atual - investido)
-		}
-	}
-	
-	resultado += "\nLucro: %.2f €".format(lucroTotal)
-	return resultado
 }
 
-fun adicionarInvestimento(investimentos: Array<Array<String>?>, nome: String, valorInvestido: Double, valorAtual: Double, ): String {
-	val indice = investimentos.indexOfFirst { it == null }
+fun lerLiquidacoes(fileName: String,liquidacoes:Array<Double>):Boolean{
 	
-	if (indice == -1) {
-		return "Já tem a carteira de investimentos completa."
-	}
-	
-	val data = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
-	
-	val quantidade = valorInvestido / valorAtual
-	
-	val investimento = arrayOf(
-		nome,
-		data,
-		valorInvestido.toString(),
-		valorAtual.toString(),
-		quantidade.toString()
-	)
-	
-	investimentos[indice] = investimento
-	
-	return "Investimento adicionado com sucesso"
+	return false
 }
+fun guardarLiquidacoes(fileName:String,liquidacoes:Array<Double>){
 
-fun editarInvestimento(investimentos: Array<Array<String>?>, nome: String, valor: Double): String {
+}
+fun lerInvestimentos(fileName:String,investimentos:Array<Array<String>?>):Boolean{
+	return false
+	
+}
+fun consultarInvestimentos(investimentos: Array<Array<String>?>,
+                           configuracoes: Array<String>, liquidacoes: Array<Double>): String{
+	return ""
+	
+}
+fun adicionarInvestimento(investimentos: Array<Array<String>?>, nome: String,
+                          valorInvestido: Double, valorAtual: Double): String{
+	
 	return ""
 }
-
-fun liquidarInvestimento(liquidacoes: Array<Double>, investimentos: Array<Array<String>?>, numero: Int): String {
+fun editarInvestimento(investimentos: Array<Array<String>?>, nome: String, valor: Double): String{
 	return ""
 }
-
-fun guardarInvestimentos(fileName: String, investimentos: Array<Array<String>?>) {
+fun liquidarInvestimento(liquidacoes: Array<Double>, investimentos: Array<Array<String>?>, numero: Int): String{
+	return ""
+}
+fun guardarInvestimentos(fileName:String, investimentos:Array<Array<String>?>) {
 
 }
-
 fun main() {
-	var nome: String
+	println(obterMenu())
+	
 	var moeda: String
 	var escolha: Int
+	var nomeCompleto:String
+	var valorPu:Double
 	
 	println(
 		"#####################\n" +
 				"### Investimentos ###\n" +
 				"#####################\n"
 	)
-	
-	while (true) {
-		println("Por favor indique o seu nome:")
-		nome = readln()
-		
-		println("Por favor indique a moeda da sua conta(€ ou $):")
-		moeda = readln()
-		
-		val nomeCompleto = nome.split(" ")
-		
-		
-		if (nomeCompleto.size != 2 || (moeda != "$" && moeda != "€")) {
-			println(
-				"Dados invalidos.\n" +
-						"O nome completo deve ser definido por pelomenos " +
-						"um espaço vazio e pelo menos 4 caracteres.\n" +
-						"A moeda deverá ser em $ ou €.\n"
-			)
-		}
-		else {
-			break
-		}
-	}
-	
-	println("Olá $nome\n")
-	
 	do {
 		
-		println(obterMenu())
-		print("Indica o comando que pretendes:\n ")
-		escolha = readln().toInt()
+		print("Por favor indeque o seu nome:\n")
+		nomeCompleto = readln()
+		print("Por favor indique a moeda da sua conta(€ ou $):\n")
+		moeda = readln()
 		
-		when (escolha) {
-			1 -> {
-				consultarInvestimentos(investimentos, configuracoes, liquidacoes)
-			}
-			2 -> {
-				println("Nome do Investimento:")
-				var nomeInvestimento = readln().toString()
-				
-				println("Valor investido:")
-				var ValorInvestido = readln().toDouble()
-				
-				println("Valo atual (PU)")
-				var ValorPu = readln().toDouble()
-				
-				var mensagem = adicionarInvestimento(investimentos, nomeInvestimento, ValorInvestido, ValorPu )
-				
-				println(mensagem)
-			}
-			
-			3 -> {
-				println("Voce escolheu Editar investimento.\n menu em desenvolvimento")
-			}
-			
-			4 -> {
-				println("Voce escolheu liquidar investimento. \n menu em desenvolvimento")
-			}
-			
-			5 -> {
-				println("Voce escolheu Guardar. \n menu em desenvolvimento")
-			}
-			
-			9 -> {
-				println("Voce escolheu configuracoes. \n menu em desenvolvimento")
-			}
+		if ((nomeCompleto.length < 2) || ((moeda != "€") && (moeda != "$"))) {
+			println("Dados inválidos.\n" +
+					"Nome completo deve ser definido por pelomenos dois nomes e ter pelomenos um espaco vazio e pelo menos 4 caracteres.\n" +
+					"A moeda deverá ser € ou \n.")
 		}
 		
-		println("(prima enter para voltar ao menu.)")
-		readln()
+	}while ((nomeCompleto.length < 2) || ((moeda != "€") && (moeda != "$")))
+	
+	val primeiroNome = nomeCompleto.split("\\s+".toRegex())[0]
+	
+	println("Olá $primeiroNome\n")
+	while (true)
 		
-	}
-	while (escolha != 0)
+		
+		do {
+			
+			println(obterMenu())
+			print("Indica o comando que pretendes:\n ")
+			escolha = readln().toInt()
+			
+			when (escolha) {
+				1 -> {
+					consultarInvestimentos(investimentos, configuracoes, liquidacoes)
+				}
+				
+				2 -> {
+					var nomeInvestimento=""
+					
+					do{
+						println("Adicionar investimento\n")
+						
+						println("Nome do Investimento:")
+						nomeInvestimento = readln().toString()
+						if (nomeInvestimento.length < 3) {
+							println("Nome inválido. o nome apenas pode  conter letras e tem de ter no minimo 3 caracteres.")
+						}
+					}while (nomeInvestimento.length < 3)
+					
+					println("Valor investido:")
+					var valorInvestido = readln().toDouble()
+					
+					println("Valo atual (PU)")
+					valorPu = readln().toDouble()
+					
+					var mensagem = adicionarInvestimento(investimentos, nomeInvestimento, valorInvestido, valorPu)
+					println(mensagem)
+					
+				}
+				3 -> {
+					println("Editar investimento\n")
+					println("Nome do investimento:")
+					var nomeInvestimento= readln().toString()
+					println("Valor atual (PU):")
+					valorPu= readln().toDouble()
+					
+					//if (editarInvestimento != nomeInvestimento && valorPu != valorAtual) {
+					println("Nao existe investimentos em carteira com esse nome.")
+					
+				}
+				
+				4 -> {
+					println("Voce escolheu liquidar investimento. \n menu em desenvolvimento")
+				}
+				
+				5 -> {
+					println("Voce escolheu Guardar. \n menu em desenvolvimento")
+				}
+				
+				9 -> {
+					println("Voce escolheu configuracoes. \n menu em desenvolvimento")
+				}
+			}
+			
+			println("(prima enter para voltar ao menu.)")
+			readln()
+			
+		}
+		while (escolha != 0)
 	
 	println("Adeus e bons investimentos")
 }
-
-
-
-
 
 
 
